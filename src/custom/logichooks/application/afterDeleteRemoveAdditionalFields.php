051120e99704e5975ec6452028dd77630f397961
Enrico Simonetti <enrico.simonetti@gmail.com>
2022-03-16 12:55:08 +1100

    Create afterDeleteRemoveAdditionalFields.php

---
+++ src/custom/logichooks/application/afterDeleteRemoveAdditionalFields.php
@@ -0,0 +1,16 @@
+<?php
+
+class afterDeleteRemoveAdditionalFields
+{
+    public function removeAdditionalFields($bean, $event, $arguments)
+    {
+        // after delete, make the field null if it is there
+        if (isset($bean->integration1_id)) {
+            $qb = \DBManagerFactory::getConnection()->createQueryBuilder();
+            $qb->update($bean->table_name)
+                ->set('integration1_id', 'NULL')
+                ->where($qb->expr()->eq('id', $qb->createPositionalParameter($bean->id)))
+                ->execute();
+        }
+    }
+}
